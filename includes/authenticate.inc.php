93a8a991c227b03e498fba9b3d5552148745f586
beshsal <beshsaleh@gmail.com>
2019-01-13 16:49:41 -0500

    add code to automatically create author profile

--- includes/authenticate.inc.php
+++ includes/authenticate.inc.php
@@ -4,7 +4,7 @@ $username = $conn->real_escape_string($username);
 $password = $conn->real_escape_string($password);
 
 // Get the user's data.
-$query = "SELECT salt, firstname, lastname, username, password, role, email FROM users WHERE username = ?";
+$query = "SELECT salt, user_id, firstname, lastname, username, password, role, email FROM users WHERE username = ?";
 
 // Initialize the prepared statement.
 $stmt = $conn->stmt_init();
@@ -16,7 +16,7 @@ $stmt = $conn->prepare($query);
 $stmt->bind_param("s", $username);
 
 // Bind the results of the query to new variables for each field.
-$stmt->bind_result($salt, $dbFname, $dbLname, $dbUname, $dbPwd, $dbRole, $dbEmail);
+$stmt->bind_result($salt, $dbUid, $dbFname, $dbLname, $dbUname, $dbPwd, $dbRole, $dbEmail);
 
 // Execute the statement.
 $stmt->execute();
@@ -24,19 +24,50 @@ $stmt->execute();
 // Fetch the resulting values of the executed the statement.
 $stmt->fetch();
 
-// Added below to fix the "Commands out of sync" error; this frees the db resource
+// Added below to fix the "Commands out of sync" error; this frees the db resource.
 $stmt->free_result();
 
 // Encrypt the password entered by the user by combining it with a salt and passing it to sha1(); then check if it matches 
 // the stored version of the password, which was also encrypted; if they match, create a session variable to indicate 
 // a successful sign-in and to obtain the time the session began.
 if (sha1($password . $salt) == $dbPwd) {
+  $_SESSION["user_id"]       = $dbUid; // user_id needed for creating an author profile for the admin/author if one doesn't already exist
   $_SESSION["firstname"]     = $dbFname;    
   $_SESSION["lastname"]      = $dbLname;    
   $_SESSION["username"]      = $dbUname;    
   $_SESSION["role"]          = $dbRole;    
   $_SESSION["email"]         = $dbEmail;   
   $_SESSION["authenticated"] = "Logged in without a hitch.";
+  
+  // If the admin or author doesn't have an author profile, create one.
+  if (isset($_SESSION["authenticated"])) { 
+      if ($_SESSION["role"] == "admin" || $_SESSION["role"] == "author") {
+        $checkProf = $conn->query("SELECT * FROM auth_profile WHERE username = '" . $_SESSION["username"] . "' ");
+        confirmQuery($checkProf);
+
+      if ($checkProf->num_rows == 0) {
+        // Create the full name for the author's profile (auth_profile).
+        $full = $_SESSION["firstname"] . " " . $_SESSION["lastname"];
+        $insertProf = "INSERT INTO auth_profile (user_id, username, firstname, lastname, fullname, email, role) VALUES (?, ?, ?, ?, ?,?, ?)";
+        if (!($stmt->prepare($insertProf))) {
+            echo "Prepare failed: (" . $conn->errno . ") " . $conn->error;
+        }
+        if (!$stmt->bind_param("issssss", 
+                               $_SESSION["user_id"], 
+                               $_SESSION["username"], 
+                               $_SESSION["firstname"], 
+                               $_SESSION["lastname"], 
+                               $full, 
+                               $_SESSION["email"], 
+                               $_SESSION["role"])) {
+            echo "Binding parameters failed: (" . $stmt->errno . ") " . $stmt->error;
+        }
+        if (!$stmt->execute()) {
+            echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
+        }  
+      }
+    }
+  }
   
   // Get the time the session started.
   $_SESSION["start"] = time();
